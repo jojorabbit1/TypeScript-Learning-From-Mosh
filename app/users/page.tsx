3d227fe8c921d64ecfe3cba48ca3c10c5393bdcd
Anuj Sharma <cloudu07@gmail.com>
2023-10-16 22:33:05 +0530

    Data Fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,23 @@
+import { link } from "fs";
 import React from "react";
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: String;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>User</h1>
+      {users.map((user) => (
+        <li key={user.id}> {user.name}</li>
+      ))}
+    </>
+  );
 };
 
 export default UsersPage;
