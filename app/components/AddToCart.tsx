67e918d1a3c4ad56d6bf69e7db35141021059f42
Anuj Sharma <cloudu07@gmail.com>
2023-10-19 00:06:41 +0530

    daisy Ui

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -4,7 +4,12 @@ import React from "react";
 const AddToCart = () => {
   return (
     <div>
-      <button onClick={() => console.log("Got It")}>Add To Cart</button>
+      <button
+        className="btn btn-secondary"
+        onClick={() => console.log("Got It")}
+      >
+        Add To Cart
+      </button>
     </div>
   );
 };
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 interface User {
   id: number;
   name: String;
+  email: String;
 }
 
 const UsersPage = async () => {
@@ -16,9 +17,25 @@ const UsersPage = async () => {
     <>
       <h1>User</h1>
       <p> {new Date().toLocaleTimeString()}</p>
-      {users.map((user) => (
-        <li key={user.id}> {user.name}</li>
-      ))}
+      <table className="table table-xs">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+        {/* {users.map((user) => (
+          <li key={user.id}> {user.name}</li>
+        ))} */}
+      </table>
     </>
   );
 };
