5d5197758a230e7bc6b962685427c74dfb1fa4cc
Anuj Sharma <cloudu07@gmail.com>
2023-10-21 14:48:31 +0530

    updating an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -15,3 +15,26 @@ export function GET(request: NextRequest, { params: { id } }: Props) {
 
   return NextResponse.json({ id: 1, name: "Anuj" });
 }
+
+export async function PUT(request: NextRequest, { params: { id } }: Props) {
+  //Validate the request body
+  // if invalid , return 404
+  // fetch the user with the given id
+  // if doesn't exist , return 404
+  //update the user
+  // return the udate user
+
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  }
+
+  if (id > 10) {
+    return NextResponse.json(
+      { error: "Invalid User Id | User Not Found" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({ id: 1, name: body.name });
+}
