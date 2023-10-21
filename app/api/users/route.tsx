9201834675e165297f236e9252b5d714bd037b76
Anuj Sharma <cloudu07@gmail.com>
2023-10-21 14:30:50 +0530

    creating an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,12 @@ export function GET(request: NextRequest) {
     { id: 2, name: "Jojo" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  if (!body.name) {
+    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  }
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
